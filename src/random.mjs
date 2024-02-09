6055b24faa29047d3e6a8892d3d4de055f887f5e
steunix <rs4000@gmail.com>
2024-02-09 12:53:00 +0100

    Use UUIDv7 instead of ULID. Closes #6

--- src/random.mjs
+++ src/random.mjs
@@ -4,12 +4,15 @@
  * @author Stefano Rivoir <rs4000@gmail.com>
  */
 
-import { ulid } from 'ulid'
+import { uuidv7obj } from 'uuidv7'
+import { CrockfordBase32 } from 'crockford-base32'
 
 /**
  * Returns unique random string
  * @returns {string} Random string
  */
 export function randomId() {
-  return ulid()
+  const rand = uuidv7obj().bytes
+  const enc = CrockfordBase32.encode(Buffer.from(rand))
+  return enc
 }
