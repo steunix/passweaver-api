7514d9df47bd873c7cfce2dac7c87e29fad98e5b
steunix <rs4000@gmail.com>
2024-07-16 08:53:47 +0200

    Logging: add response time

--- passweaver-api.mjs
+++ passweaver-api.mjs
@@ -68,7 +68,10 @@ const logAccess = RFS.createStream(`${cfg.log_dir}/passweaver-api-access.log`, {
   interval: "1d",
   rotate: 14
 })
-app.use(Morgan('common', { stream: logAccess }))
+app.use(
+  Morgan(`:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :total-time[0]`,
+  { stream: logAccess })
+)
 
 // Log errors
 const logErrors = RFS.createStream(`${cfg.log_dir}/passweaver-api-errors.log`, {
