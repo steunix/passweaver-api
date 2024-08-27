3b9f353a122058b771403cf8089e3a572985aa8f
steunix <rs4000@gmail.com>
2024-08-27 09:57:09 +0200

    Remove uuid format, since a blank field results in a validation error

--- api/v1/controllers/items.mjs
+++ api/v1/controllers/items.mjs
@@ -22,7 +22,7 @@ import DB from '../../../lib/db.mjs'
 const createSchema = {
   "id": "create",
   "properties": {
-    "type" : { "type": "string", "format": "uuid" },
+    "type" : { "type": "string" },
     "title" : { "type": "string", "maxLength": 200 },
     "data" : { "type": "string" },
     "metadata": { "type": "string" }
@@ -32,7 +32,7 @@ const createSchema = {
 const updateSchema = {
   "id": "update",
   "properties": {
-    "type" : { "type": "string", "format": "uuid" },
+    "type" : { "type": "string" },
     "title" : { "type": "string", "maxLength": 200 },
     "data" : { "type": "string" },
     "metadata": { "type": "string" },
