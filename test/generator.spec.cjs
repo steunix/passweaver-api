f95cf0de0d6c58bc44d82890e3e181a069829769
steunix <stefano.rivoir@tesisquare.com>
2024-12-02 22:45:37 +0100

    Add parameter to generate random password without symbols Fixes #102

--- test/generator.spec.cjs
+++ test/generator.spec.cjs
@@ -3,9 +3,18 @@
 require('./common.cjs')
 
 describe('Generator', function () {
-  it('Generator', async () => {
+  it('Generate password with symbols', async () => {
     const res1 = await agent
       .get(`${global.host}/api/v1/util/generatepassword`)
+      .set('Authorization', `Bearer ${global.userJWT}`)
+      .catch(v => v)
+
+    assert.strictEqual(res1.status, 200)
+  })
+
+  it('Generate password without symbols', async () => {
+    const res1 = await agent
+      .get(`${global.host}/api/v1/util/generatepassword?symbols=false`)
       .set('Authorization', `Bearer ${global.userJWT}`)
       .catch(v => v)
 
