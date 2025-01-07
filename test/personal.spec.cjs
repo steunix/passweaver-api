638cd6ab2b7b89149dc0c9d17ddd1777fbca17f1
Stefano Rivoir <rs4000@gmail.com>
2025-01-07 21:31:40 +0100

    Add tests for personal items (#146)
    Fixes #145

--- test/personal.spec.cjs
+++ test/personal.spec.cjs
@@ -30,4 +30,39 @@ describe('Personal folders', function () {
     assert.strictEqual(res2.status, 200)
     assert(Object.hasOwn(res2.body.data, 'jwt'))
   })
+
+  it('Create, update and remove personal item', async () => {
+    const res1 = await agent
+      .post(`${global.host}/api/v1/personal/unlock`)
+      .set('Authorization', `Bearer ${global.userJWT}`)
+      .send({ password: '123' })
+      .catch(v => v)
+
+    assert.strictEqual(res1.status, 200)
+    assert(Object.hasOwn(res1.body.data, 'jwt'))
+
+    const res2 = await agent
+      .post(`${global.host}/api/v1/folders/user1/items`)
+      .set('Authorization', `Bearer ${res1.body.data.jwt}`)
+      .send(global.itemCreateData)
+      .catch(v => v)
+
+    assert.strictEqual(res2.status, 201)
+    const itemid = res2.body.data.id
+
+    const res3 = await agent
+      .patch(`${global.host}/api/v1/items/${itemid}`)
+      .set('Authorization', `Bearer ${res1.body.data.jwt}`)
+      .send({ metadata: 'test' })
+      .catch(v => v)
+
+    assert.strictEqual(res3.status, 200)
+
+    // Cleanup
+    const res4 = await agent
+      .delete(`${global.host}/api/v1/items/${itemid}`)
+      .set('Authorization', `Bearer ${res1.body.data.jwt}`)
+      .catch(v => v)
+    assert.strictEqual(res4.status, 200)
+  })
 })
