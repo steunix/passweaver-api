369ddbe3567c610391eac416b00382a94758c81b
steunix <stefano.rivoir@tesisquare.com>
2025-01-15 00:37:27 +0100

    Unify Prisma seed files Fixes #157

--- prisma/seed.cjs
+++ prisma/seed.cjs
@@ -1,107 +1,109 @@
+const { parseArgs } = require('node:util')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const Const = {
-  PW_USER_ADMINID: "0",
-  PW_FOLDER_ROOTID: "0",
-  PW_FOLDER_PERSONALROOTID: "P",
-  PW_GROUP_ROOTID: "0",
-  PW_GROUP_ADMINSID: "A",
-  PW_GROUP_EVERYONEID: "E"
+  PW_USER_ADMINID: '0',
+  PW_FOLDER_ROOTID: '0',
+  PW_FOLDER_PERSONALROOTID: 'P',
+  PW_GROUP_ROOTID: '0',
+  PW_GROUP_ADMINSID: 'A',
+  PW_GROUP_EVERYONEID: 'E'
 }
 
 /**
  * Database default initialization
  */
-async function main() {
+async function main () {
+  console.log('Creating base data...')
   let id
 
   /** FOLDERS  */
   // Root
   id = Const.PW_FOLDER_ROOTID
-  const rootFolder = await prisma.folders.upsert({
-      where: { id: id },
-      update: {},
-      create: {
-          id: id,
-          description: "Root folder",
-          parent: null
-      }
+  await prisma.folders.upsert({
+    where: { id },
+    update: {},
+    create: {
+      id,
+      description: 'Root folder',
+      parent: null
+    }
   })
 
   // Personal folders root
   id = Const.PW_FOLDER_PERSONALROOTID
-  const personalFolders = await prisma.folders.upsert({
-      where: { id: id },
-      update: {},
-      create: {
-          id: id,
-          description: "Personal folders",
-          parent: "0"
-      }
+  await prisma.folders.upsert({
+    where: { id },
+    update: {},
+    create: {
+      id,
+      description: 'Personal folders',
+      parent: '0'
+    }
   })
 
   /** GROUPS */
   // Root
   id = Const.PW_GROUP_ROOTID
-  const rootGroup = await prisma.groups.upsert({
-    where: { id: id },
+  await prisma.groups.upsert({
+    where: { id },
     update: {},
     create: {
-        id: "0",
-        description: "Root group",
-        parent: null
+      id: '0',
+      description: 'Root group',
+      parent: null
     }
   })
 
   // Admins
   id = Const.PW_GROUP_ADMINSID
-  const adminsGroup = await prisma.groups.upsert({
-    where: { id: id },
+  await prisma.groups.upsert({
+    where: { id },
     update: {},
     create: {
-        id: id,
-        description: "Admins",
-        parent: "0"
+      id,
+      description: 'Admins',
+      parent: '0'
     }
   })
 
   // Everyone
   id = Const.PW_GROUP_EVERYONEID
-  const everyoneGroup = await prisma.groups.upsert({
-    where: { id: id },
+  await prisma.groups.upsert({
+    where: { id },
     update: {},
     create: {
-        id: id,
-        description: "Everyone",
-        parent: "0"
+      id,
+      description: 'Everyone',
+      parent: '0'
     }
   })
 
   /** USERS */
   // Admin
   id = Const.PW_USER_ADMINID
-  const admin = await prisma.users.upsert({
-    where: { id: id },
+  prisma.users.upsert({
+    where: { id },
     update: {},
     create: {
-        id: id,
-        login: "admin",
-        lastname: "admin",
-        firstname: "",
-        locale: "en_US",
-        authmethod: "local",
-        email: "admin",
-        secret: "$2a$12$YIWFVQ9cU6Xv9Jf4jOz9VeyS7APLpAqmXqKM7ap8CybvJSc7ldLba",
-        secretexpiresat: new Date(2050,1,1),
-        personalsecret: null,
-        active: true
+      id,
+      login: 'admin',
+      lastname: 'admin',
+      firstname: '',
+      locale: 'en_US',
+      authmethod: 'local',
+      email: 'admin',
+      secret: '$2a$12$YIWFVQ9cU6Xv9Jf4jOz9VeyS7APLpAqmXqKM7ap8CybvJSc7ldLba',
+      secretexpiresat: new Date(2050, 1, 1),
+      personalsecret: null,
+      active: true
     }
   })
 
   /** USERS GROUP ASSOCIATION */
   // Admin in Admins
-  const gu1 = await prisma.groupsmembers.create({
+  await prisma.groupsmembers.create({
     data: {
       groupid: Const.PW_GROUP_ADMINSID,
       userid: Const.PW_USER_ADMINID
@@ -109,7 +111,7 @@ async function main() {
   })
 
   // Admin in Everyone
-  const gu2 = await prisma.groupsmembers.create({
+  await prisma.groupsmembers.create({
     data: {
       groupid: Const.PW_GROUP_EVERYONEID,
       userid: Const.PW_USER_ADMINID
@@ -117,7 +119,7 @@ async function main() {
   })
 
   /** FOLDER GROUP PERMISSIONS */
-  const fg0 = await prisma.folderspermissions.create({
+  await prisma.folderspermissions.create({
     data: {
       folderid: Const.PW_FOLDER_ROOTID,
       groupid: Const.PW_GROUP_ADMINSID,
@@ -127,16 +129,96 @@ async function main() {
   })
 
   /** ITEMS TYPE */
-  const it0 = await prisma.itemtypes.create({
+  await prisma.itemtypes.create({
     data: {
-      description: "default",
-      icon: "key"
+      description: 'default',
+      icon: 'key'
     }
   })
+
+  /** DEVELOPMENT DATA */
+  const options = {
+    environment: { type: 'string' }
+  }
+  const {
+    values: { environment }
+  } = parseArgs({ options })
+
+  if (environment === 'development') {
+    console.log('Creating development data...')
+
+    // Sample folders
+    for (let i = 1; i <= 3; i++) {
+      id = `sample${i}`
+      await prisma.folders.upsert({
+        where: { id },
+        update: {},
+        create: {
+          id,
+          description: `Sample folder ${i}`,
+          parent: Const.PW_FOLDER_ROOTID
+        }
+      })
+    }
+
+    // Sample users
+    for (let i = 1; i >= 2; i++) {
+      id = `user${i}`
+      await prisma.users.upsert({
+        where: { id },
+        update: {},
+        create: {
+          id,
+          login: id,
+          lastname: id,
+          firstname: '',
+          locale: 'en_US',
+          authmethod: 'local',
+          email: id,
+          secret: '$2a$12$YIWFVQ9cU6Xv9Jf4jOz9VeyS7APLpAqmXqKM7ap8CybvJSc7ldLba',
+          secretexpiresat: new Date(2050, 1, 1),
+          personalsecret: null,
+          active: true
+        }
+      })
+
+      // Add user to Everyone
+      await prisma.groupsmembers.create({
+        data: {
+          groupid: Const.PW_GROUP_EVERYONEID,
+          userid: id
+        }
+      })
+
+      // Personal folders
+      await prisma.folders.upsert({
+        where: { id },
+        update: {},
+        create: {
+          id,
+          description: `Personal folder for ${id}`,
+          parent: Const.PW_FOLDER_PERSONALROOTID,
+          personal: true,
+          userid: id
+        }
+      })
+    }
+
+    // Folder permissions
+    await prisma.folderspermissions.create({
+      data: {
+        folderid: 'sample1',
+        groupid: Const.PW_GROUP_EVERYONEID,
+        read: true,
+        write: true
+      }
+    })
+  }
 }
 
 main()
   .then(async () => {
+    console.log('Data successfully created or updated')
     await prisma.$disconnect()
   })
   .catch(async (e) => {
