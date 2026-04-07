343e0a933a2ad18769961caeb2f707dae4200caa
Stefano Rivoir <rs4000@gmail.com>
2026-04-07 16:53:32 +0200

    Optional LDAP user provisioning (#533)
    Fixes #406

--- model/user.mjs
+++ model/user.mjs
@@ -7,6 +7,10 @@
  */
 
 import DB from '../lib/db.mjs'
+import * as Crypt from '../lib/crypt.mjs'
+import * as Events from '../lib/event.mjs'
+import * as Const from '../lib/const.mjs'
+import { newId } from '../lib/id.mjs'
 
 /**
  * Return true if the user exists
@@ -39,4 +43,59 @@ export async function groups (user) {
     array.push(rec.groups)
   }
   return array
+}
+
+/**
+ * Create a user
+ * @param {string} login User login
+ * @param {string} firstName User first name
+ * @param {string} lastName User last name
+ * @param {string} email User email
+ * @param {string} locale User locale
+ * @param {string} authMethod User authentication method
+ * @param {string} secret User secret
+ */
+export async function provisionLDAP (login, firstName, lastName, email, locale, authMethod, secret) {
+  // Creates user
+  const newUserId = newId()
+  await DB.$transaction(async (tx) => {
+    const hash = await Crypt.hashPassword(secret)
+    await DB.users.createMany({
+      data: [{
+        id: newUserId,
+        login: login.toLowerCase(),
+        firstname: firstName,
+        lastname: lastName,
+        locale: locale || 'en_US',
+        authmethod: authMethod,
+        email: email.toLowerCase(),
+        secret: hash,
+        secretexpiresat: new Date(2050, 12, 31, 23, 59, 59)
+      }]
+    })
+
+    // Creates personal folder
+    const newFolderId = newId()
+    await DB.folders.createMany({
+      data: [{
+        id: newFolderId,
+        description: login,
+        parent: Const.PW_FOLDER_PERSONALROOTID,
+        personal: true,
+        userid: newUserId
+      }]
+    })
+
+    // Add user to 'Everyone' group
+    await DB.groupsmembers.createMany({
+      data: [{
+        groupid: Const.PW_GROUP_EVERYONEID,
+        userid: newUserId
+      }]
+    })
+  })
+
+  await Events.add(newUserId, Const.EV_ACTION_LDAPPROVISION, Const.EV_ENTITY_USER, newUserId)
+
+  return newUserId
 }
